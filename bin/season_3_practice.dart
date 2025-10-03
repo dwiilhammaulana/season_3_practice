8c645d749873aee167a4777bdbc4924029333215
dwiilhammaulana <1123150008@global.ac.id>
2025-10-03 21:23:00 +0700

    -

--- bin/season_3_practice.dart
+++ bin/season_3_practice.dart
@@ -1,5 +1,31 @@
-import 'package:season_3_practice/season_3_practice.dart' as season_3_practice;
+class Car {
+  String color;
+  int price;
+  int km;
+  String model;
 
-void main(List<String> arguments) {
-  print('Hello world: ${season_3_practice.calculate()}!');
+  Car({
+    required this.color,
+    required this.price,
+    required this.km,
+    required this.model,
+  });
+
+  void start() => print('$model started');
+  void forward() => print('$model moving forward');
+  void backward() => print('$model moving backward');
+  void stop() => print('$model stopped');
+
+  @override
+  String toString() => 'Car(color:$color, price:$price, km:$km, model:$model)';
+}
+
+void main() {
+  // Membuat OBJECT dari CLASS Car (instance)
+  final myCar = Car(color: 'red', price: 23000, km: 1200, model: 'Audi');
+
+  print(myCar);      // Car(color:red, price:23000, km:1200, model:Audi)
+  myCar.start();     // Audi started
+  myCar.forward();   // Audi moving forward
+  myCar.stop();      // Audi stopped
 }
