98644212a73424f17a473f14ea39a952612b8d2b
dwiilhammaulana <1123150008@global.ac.id>
2025-10-03 21:25:09 +0700

    blabla

--- bin/season_3_practice.dart
+++ bin/season_3_practice.dart
@@ -1,31 +1,12 @@
-class Car {
-  String color;
-  int price;
-  int km;
-  String model;
-
-  Car({
-    required this.color,
-    required this.price,
-    required this.km,
-    required this.model,
-  });
-
-  void start() => print('$model started');
-  void forward() => print('$model moving forward');
-  void backward() => print('$model moving backward');
-  void stop() => print('$model stopped');
-
-  @override
-  String toString() => 'Car(color:$color, price:$price, km:$km, model:$model)';
+Future<String> getData() async {
+  // simulasi delay 2 detik
+  await Future.delayed(Duration(seconds: 2));
+  return 'Data berhasil diambil!';
 }
 
-void main() {
-  // Membuat OBJECT dari CLASS Car (instance)
-  final myCar = Car(color: 'red', price: 23000, km: 1200, model: 'Audi');
-
-  print(myCar);      // Car(color:red, price:23000, km:1200, model:Audi)
-  myCar.start();     // Audi started
-  myCar.forward();   // Audi moving forward
-  myCar.stop();      // Audi stopped
+void main() async {
+  print('Mulai fetch...');
+  String hasil = await getData();
+  print(hasil); // Data berhasil diambil!
+  print('Selesai');
 }
